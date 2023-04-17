3451215ff75ead72b53530d0b9bfcf7902b761c2
aimee <aimeemusabwe@gmail.com>
2023-04-17 17:39:43 +0200

    a web page about workit

---
+++ index.js
@@ -0,0 +1,26 @@
+function appear(){
+    document.getElementById("two").style.textDecoration="underline";
+    document.getElementById("two").style.textDecorationColor="aquamarine";
+
+}
+function disappear(){
+    document.getElementById("two").style.textDecoration=" none";
+
+}
+function apply(){
+    document.getElementById("apply").style.textDecoration="underline";
+    document.getElementById("apply").style.textDecorationColor="aquamarine";
+}
+function disapply(){
+    document.getElementById("apply").style.textDecoration=" none";
+}
+function more(){
+    document.getElementById("more").style.backgroundColor="white";
+}
+function newPhoto(){
+    this.setAtrribute("src","./js practice/pexels-photo-13603893.jpeg")
+}
+function photo(){
+    
+}
+
